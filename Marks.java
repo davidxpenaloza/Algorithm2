e8e04971f4704b9696fd54523710e68c70973535
davidxpenaloza <d.penaloza.aponte.10@student.scu.edu.au>
2024-11-10 23:02:39 +1000

    Comments

--- Marks.java
+++ Marks.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner; //import the Scanner class for user input
 
 /**
  * Write a description of class Marks here.
@@ -6,68 +6,66 @@ import java.util.Scanner;
  * @author (David Penaloza)
  * @version (V1.0)
  */
-public class Marks
+public class Marks //A pucblic class named Marks is defined
 {
-    // instance variables 
 
     /**
      * Constructor for objects of class Marks
      */
-    public static void main(String[] args)
+    public static void main(String[] args) //The main method, is the entry point of Java application.
     {
-        int numberOfStudents = 30;
-        float[] mark = new float[numberOfStudents];
-        Scanner scanner = new Scanner(System.in);
+        int numberOfStudents = 30; //The number of students to 30 is inicializated.
+        float[] mark = new float[numberOfStudents];//An array is created to store marks for each student, where each value is float.
+        Scanner scanner = new Scanner(System.in);//A scanner object was created for user input
         
-        System.out.println("Give marks to 30 students");
+        System.out.println("Give marks to 30 students");//Prompt user to enter marks
+        System.out.println("Enter the assignment name: "); //Prompt user to enter the assignment name
+        String assignmentName = scanner.nextLine(); //Read the assigenment name from user input
+        System.out.println("Enter the marks: \n"); //Prompt user to enter marks for the students and change of Line to the next
         
-        //assignment name
-        System.out.println("Enter the assignment name: ");
-        String assignmentName = scanner.nextLine();
-        System.out.println("Enter the marks: \n");
-      
-        for(int i=0; i<numberOfStudents; i++){
+        //Get marks from the user with validation
+        for(int i=0; i<numberOfStudents; i++){//create variable i as a counter, the next condition is a loop's condition, finally is a increment the value of i by 1 after each iteration.
             
-            float temp;
-
+            float temp; //The temporary variable temp is defined to store current input
+            //Started to create do-while loop, the code block will excecute once, before analizing if condition is not false.
             do{
-                temp = scanner.nextFloat();
-                if(temp<0 || temp>30){
+                temp = scanner.nextFloat(); //read float input for the marks
+                if(temp<0 || temp>30){ //Condition
 
-                   System.out.println("Invalide marks,please enter again");    
+                   System.out.println("Invalide marks,please enter again"); //inform user wrong value started     
                 }
-                } while (temp<0 || temp>30);
-                mark[i] = temp;
+                } while (temp<0 || temp>30); //Condition is repeated until correct input is started
+                mark[i] = temp; // In the array is store the valid mark
             
             }
         //Showed user marks
-        System.out.println("Entered marks");
-        for(int i=0; i<numberOfStudents; i++){
-            System.out.println(mark[i]);
+        System.out.println("Entered marks"); //Is the header for marks put on view
+        for(int i=0; i<numberOfStudents; i++){ //create variable "j" as a counter, the next condition is a loop's condition, finally is a increment the value of "j" by 1 after each iteration.
+            System.out.println(mark[i]); //The mark of each student is printed
             
         }
         
-        //Calculate t markshe mean (average)
-        float total = 0;
-        for(int j = 0; j<numberOfStudents; j++){
-            total = total + mark[j];
+        //Calculate of mean (average)
+        float total = 0; // The variable total is initialize in cero and defined as float
+        for(int j = 0; j<numberOfStudents; j++){ //create variable "j" as a counter, the next condition is a loop's condition, finally is a increment the value of "j" by 1 after each iteration.
+            total = total + mark[j]; // Add the mark of the current student to "total"
         
         }
-        float mean;
-        mean = total / numberOfStudents;
+        float mean;//Declare a variable "mean" to store the average of the marks
+        mean = total / numberOfStudents; //Calculate the mean 
         
-        float totalsumdiff =0;
-        for(int t = 0; t<numberOfStudents; t++){
-            totalsumdiff = totalsumdiff + (mark[t]-mean)*(mark[t]-mean)/numberOfStudents;
+        float totalsumdiff =0; //Initialize totalsumdiff to 0
+        for(int t = 0; t<numberOfStudents; t++){ //Create a counter t and a loop through all students
+            totalsumdiff = totalsumdiff + (mark[t]-mean)*(mark[t]-mean)/numberOfStudents;//Calculate the squared difference from the mean for each mark, then accumulate.
         
         }
         
-        float standardDeviation;
-        standardDeviation = (float) Math.sqrt(totalsumdiff / (numberOfStudents-1));
+        float standardDeviation; //Declare the variable standardDeviation as float
+        standardDeviation = (float) Math.sqrt(totalsumdiff / (numberOfStudents-1));//Calculate standard deviation
         
-        System.out.println("\nAssignment Name: "+ assignmentName);
-        System.out.println("The mean value of total Mark is: " + mean);
-        System.out.println("Standard Deviation: "+ standardDeviation); 
+        System.out.println("\nAssignment Name: "+ assignmentName); //The assignment name is printed
+        System.out.println("The mean value of total Mark is: " + mean); //The mean value is printed
+        System.out.println("Standard Deviation: "+ standardDeviation); //The minimun value is printed
         
     }
 }
